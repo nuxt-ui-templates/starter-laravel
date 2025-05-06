8a1830a80221beeff1e3927221095e6adc69691b
Benjamin Canac <canacb1@gmail.com>
2025-05-06 16:14:35 +0200

    chore(types): update

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,21 +1,8 @@
 import type { PageProps } from '@inertiajs/core'
-import type { LucideIcon } from 'lucide-vue-next'
 import type { Config } from 'ziggy-js'
 
 export interface Auth {
   user: User
-}
-
-export interface BreadcrumbItem {
-  title: string
-  href: string
-}
-
-export interface NavItem {
-  title: string
-  href: string
-  icon?: LucideIcon
-  isActive?: boolean
 }
 
 export interface SharedData extends PageProps {
@@ -35,5 +22,3 @@ export interface User {
   created_at: string
   updated_at: string
 }
-
-export type BreadcrumbItemType = BreadcrumbItem
